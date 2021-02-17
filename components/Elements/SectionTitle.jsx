06510a37dba2e11c5a8d9aa5926b2939753553ca
apexof <apexof@gmail.com>
2021-02-17 19:26:33 +0300

    fix link

--- components/Elements/SectionTitle.jsx
+++ components/Elements/SectionTitle.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import Link from './Link'
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Link, Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
     link: {

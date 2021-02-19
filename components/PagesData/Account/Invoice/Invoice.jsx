0a81253168f627930c8db2ce2c71ae36043420c2
apexof <apexof@gmail.com>
2021-02-19 19:39:00 +0300

    fix invoiche styles

--- components/PagesData/Account/Invoice/Invoice.jsx
+++ components/PagesData/Account/Invoice/Invoice.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import cx from 'clsx';
+import { useThemeContext } from '../../../ThemeSwitcher/MyThemeProvider';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Link from '../../../Elements/Link';
 import IntlMessages from '../../../KouttComponents/IntlMessages';
@@ -42,105 +44,117 @@ const useStyles = makeStyles(theme => ({
     },
     table: {
         minWidth: 700,
+    },
+    thinText: {
+        fontWeight: 300,
+    },
+    borderHighlight: {
+        padding: 10,
+        borderRadius: 3,
+        color: theme.palette.primary.contrastText,
+        background: theme.palette.background.default,
+    },
+    borderPage: {
+        padding: theme.spacing(1.5),
+        border: `1px solid ${theme.palette.grey["50"]}`,
+    },
+    container: {
+        background: theme.palette.background.paper,
     }
 }));
 
 export default function Invoice() {
     const classes = useStyles();
+    const { nightMode } = useThemeContext()
 
     return (
-        <div className="invoice-page">
-            <Box className="white-btn-color">
-                {/* <SmallTitleBar
-                    title={<IntlMessages id="component.invoice" />}
-                    buttonText={<IntlMessages id="component.backToProducts" />}
-                    buttonLink="/app/ecommerce/shop"
-                /> */}
-            </Box>
-            <div className="page-space">
-                <CustomCard>
-                    <div className="main-invoice">
-                        <Grid container spacing={3} direction="row">
-                            <Grid item xs={12} sm={8}>
-                                <Box mb={5}>
-                                    <Typography variant="h2">INVOICE</Typography>
-                                </Box>
-                                <Box mb={3} className="site-logo">
-                                    <Link href="/" className="logo-mini mb-1 d-block">
+        <div className={classes.container}>
+            <CustomCard>
+                <div className={classes.borderPage}>
+                    <Grid container spacing={3} direction="row">
+                        <Grid item xs={12} sm={8}>
+                            <Box mb={5}>
+                                <Typography variant="h2">INVOICE</Typography>
+                            </Box>
+                            <Box mb={3} >
+                                <Link href="/">
+                                    {nightMode ?
+                                        <img src="/img/koutt-light.png" alt="site logo" width="80" />
+                                        :
                                         <img src="/img/koutt-dark.png" alt="site logo" width="80" />
-                                    </Link>
-                                </Box>
-                                <Box fontSize="body2.fontSize" color="text.primary" fontWeight="500">Koutt Solutions LLC
-                                    <Typography>4502  Poe Road, Conway South Carolina</Typography>
-                                    <Typography>Zip Code - 29528</Typography>
-                                    <Typography>FAX.843-234-0429</Typography>
-                                    <Typography className="text-over">Email.invoice@email.com</Typography>
-                                </Box>
-                            </Grid>
-                            <Grid item xs={12} sm={4} className="text-right">
-                                <Typography>Date:Nov 21 2010</Typography>
-                                <Typography>Invoice No. QWERTY12345TY</Typography>
-                                <Typography>Payment Due. $10.23</Typography>
-                            </Grid>
+                                    }
+                                </Link>
+                            </Box>
+                            <Box fontSize="body2.fontSize" color="text.primary" fontWeight="500">Koutt Solutions LLC
+                                    <Typography className={classes.thinText}>4502  Poe Road, Conway South Carolina</Typography>
+                                <Typography className={classes.thinText}>Zip Code - 29528</Typography>
+                                <Typography className={classes.thinText}>FAX.843-234-0429</Typography>
+                                <Typography className={cx(classes.thinText, "text-over")}>Email.invoice@email.com</Typography>
+                            </Box>
                         </Grid>
-                        <Box my={5}>
-                            <Box className="border-highlight" fontSize="body2.fontSize" color="primary.contrastText" fontWeight="500" mb="20px">Bill To:</Box>
-                            <Typography >Kenneth C Thompson</Typography>
-                            <Typography >1249  Conifer Drive, Everett, Washington</Typography>
-                            <Typography >Zip Code - 98208</Typography>
-                            <Typography >Mob.425-359-3066</Typography>
-                            <Typography className="text-over">Email.rdce7marygo@classesmail.com</Typography>
+                        <Grid item xs={12} sm={4}>
+                            <Typography className={classes.thinText}>Date:Nov 21 2010</Typography>
+                            <Typography className={classes.thinText}>Invoice No. QWERTY12345TY</Typography>
+                            <Typography className={classes.thinText}>Payment Due. $10.23</Typography>
+                        </Grid>
+                    </Grid>
+                    <Box my={5}>
+                        <Box className={classes.borderHighlight} fontSize="body2.fontSize" fontWeight="500" mb="20px">Bill To:</Box>
+                        <Typography className={classes.thinText}>Kenneth C Thompson</Typography>
+                        <Typography className={classes.thinText}>1249  Conifer Drive, Everett, Washington</Typography>
+                        <Typography className={classes.thinText}>Zip Code - 98208</Typography>
+                        <Typography className={classes.thinText}>Mob.425-359-3066</Typography>
+                        <Typography className={cx(classes.thinText, "text-over")}>Email.rdce7marygo@classesmail.com</Typography>
+                    </Box>
+                    <Box>
+                        <Box mb={4}>
+                            <TableContainer component={Paper} className="shadow1">
+                                <Table className={classes.table} aria-label="customized table">
+                                    <TableHead>
+                                        <TableRow>
+                                            <StyledTableCell>Product Name</StyledTableCell>
+                                            <StyledTableCell align="right">Price</StyledTableCell>
+                                            <StyledTableCell align="right">QTY</StyledTableCell>
+                                            <StyledTableCell align="right">Subtotal</StyledTableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {rows.map(row => (
+                                            <StyledTableRow key={row.name}>
+                                                <StyledTableCell component="th" scope="row">
+                                                    {row.name}
+                                                </StyledTableCell>
+                                                <StyledTableCell align="right">{row.price}</StyledTableCell>
+                                                <StyledTableCell align="right">{row.qty}</StyledTableCell>
+                                                <StyledTableCell align="right">{row.subtotal}</StyledTableCell>
+                                            </StyledTableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
                         </Box>
                         <Box>
-                            <Box mb={4}>
-                                <TableContainer component={Paper} className="invoice-table">
-                                    <Table className={classes.table} aria-label="customized table">
-                                        <TableHead>
-                                            <TableRow>
-                                                <StyledTableCell>Product Name</StyledTableCell>
-                                                <StyledTableCell align="right">Price</StyledTableCell>
-                                                <StyledTableCell align="right">QTY</StyledTableCell>
-                                                <StyledTableCell align="right">Subtotal</StyledTableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {rows.map(row => (
-                                                <StyledTableRow key={row.name}>
-                                                    <StyledTableCell component="th" scope="row">
-                                                        {row.name}
-                                                    </StyledTableCell>
-                                                    <StyledTableCell align="right">{row.price}</StyledTableCell>
-                                                    <StyledTableCell align="right">{row.qty}</StyledTableCell>
-                                                    <StyledTableCell align="right">{row.subtotal}</StyledTableCell>
-                                                </StyledTableRow>
-                                            ))}
-                                        </TableBody>
-                                    </Table>
-                                </TableContainer>
-                            </Box>
-                            <Box>
-                                <Box display="flex" justifyContent="flex-end" mb={3}>
-                                    <Box width={250}>
-                                        <Box display="flex" alignItems="center" fontSize="body2.fontSize" color="text.secondary" mb={1}>
-                                            <Box width="50%">Subtotal : </Box>
-                                            <Box width="50%" textAlign="right">$1,174</Box>
-                                        </Box>
-                                        <Box display="flex" alignItems="center" fontSize="body2.fontSize" color="text.secondary" mb={2}>
-                                            <Box width="50%">Vat (10%) :</Box>
-                                            <Box width="50%" textAlign="right">$232</Box>
-                                        </Box>
-                                        <Divider />
-                                        <Box pt={2} display="flex" alignItems="center" fontSize="h6.fontSize" color="primary" mb={1}>
-                                            <Box width="50%">Total :</Box>
-                                            <Box width="50%" textAlign="right">$1,406.00</Box>
-                                        </Box>
+                            <Box display="flex" justifyContent="flex-end" mb={3}>
+                                <Box width={250}>
+                                    <Box display="flex" alignItems="center" fontSize="body2.fontSize" color="text.secondary" mb={1}>
+                                        <Box width="50%">Subtotal : </Box>
+                                        <Box width="50%" textAlign="right">$1,174</Box>
+                                    </Box>
+                                    <Box display="flex" alignItems="center" fontSize="body2.fontSize" color="text.secondary" mb={2}>
+                                        <Box width="50%">Vat (10%) :</Box>
+                                        <Box width="50%" textAlign="right">$232</Box>
+                                    </Box>
+                                    <Divider />
+                                    <Box pt={2} display="flex" alignItems="center" fontSize="h6.fontSize" color="primary" mb={1}>
+                                        <Box width="50%">Total :</Box>
+                                        <Box width="50%" textAlign="right">$1,406.00</Box>
                                     </Box>
                                 </Box>
                             </Box>
                         </Box>
-                    </div>
-                </CustomCard>
-            </div>
+                    </Box>
+                </div>
+            </CustomCard>
         </div>
     );
 }
